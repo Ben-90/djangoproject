1163adc3b06523116ddc02bee597817ca07d1566
Ben-90 <mohamed.benomar@ucad.edu.sn>
2024-08-05 17:47:10 +0000

    new blog app

--- lecture3/lecture3/urls.py
+++ lecture3/lecture3/urls.py
@@ -19,8 +19,10 @@ from django.urls import path, include
 
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     path('hello/', include("hello.urls")),
     path('newyear/', include("newyear.urls")),
     path('tasks/', include('tasks.urls')),
+    path('blog/', include("blog.urls")),
 ]
