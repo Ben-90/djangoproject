f61071891f2b936ddce64bbddf82e306cc3c694d
Ben <ben-90>
2024-09-03 13:14:22 +0000

    Sur la branche master

--- lecture3/lecture3/settings.py
+++ lecture3/lecture3/settings.py
@@ -31,11 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blog',
     'tasks',
+    'blog',
     'hello',
     'newyear',
-    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
